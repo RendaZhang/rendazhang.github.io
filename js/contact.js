749f6eb0476c7861d885de85389cc5a885c665fa
张人大 <952402967@qq.com>
2020-05-16 15:58:49 +0800

    JQuery Ajax 实现 Post 请求

--- js/contact.js
+++ js/contact.js
@@ -9,38 +9,43 @@ $(function () {
 
 
     // when the form is submitted
-    // $('#contact-form').on('submit', function (e) {
+    $('#contact-form').on('submit', function (e) {
 
-    //     // if the validator does not prevent form submit
-    //     if (!e.isDefaultPrevented()) {
-    //         var url = "https://formspree.io/contact-form";
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "https://formspree.io/xlepgene";
 
-    //         // POST values in the background the the script URL
-    //         $.ajax({
-    //             type: "POST",
-    //             url: url,
-    //             data: $(this).serialize(),
-    //             success: function (data)
-    //             {
-    //                 // data = JSON object that contact.php returns
-
-    //                 // we recieve the type of the message: success x danger and apply it to the 
-    //                 var messageAlert = 'alert-' + data.type;
-    //                 var messageText = data.message;
-
-    //                 // let's compose Bootstrap alert box HTML
-    //                 var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-    //                 // If we have messageAlert and messageText
-    //                 if (messageAlert && messageText) {
-    //                     // inject the alert to .messages div in our form
-    //                     $('#contact-form').find('.messages').html(alertBox);
-    //                     // empty the form
-    //                     $('#contact-form')[0].reset();
-    //                 }
-    //             }
-    //         });
-    //         return false;
-    //     }
-    // })
+            // POST values in the background with the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                headers: {      // request headers
+                    Accept: "application/json; charset=utf-8"
+                },
+                data: $(this).serialize(),
+                dataType: 'json',
+                crossDomain: true,
+                success: function (data)
+                {
+                    // data = JSON object that formspree returns
+                    console.log(data);
+                    var ok = data.ok;
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox;
+                    if (ok) {
+                        alertBox = '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>已经发送到我的邮箱哦</div>';
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    } else {
+                        alertBox = '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>发送失败</div>';
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                    }
+                }
+            });
+            return false;
+        }
+    })
 });
